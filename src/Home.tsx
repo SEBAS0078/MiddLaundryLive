0dc92a140ccef581506b81bc5b1d8fabb4a88e3d
SEBAS0078 <sebascruz0502@gmail.com>
2025-12-08 20:08:26 -0500

    homepage , basic building select

--- src/Home.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to MiddBin</h1>
-      <p>Your laundry management solution</p>
-    </div>
-  );
-}
--- src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to MiddLaundryLive</h1>
-      <p>Your laundry management solution</p>
-    </div>
-  );
-}
